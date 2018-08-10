004f393f0fff9992e3f256b0be8b4fec5eb0caf5
meghanasarikonda <meghana94987@gmail.com>
2018-08-11 05:15:35 +0530

    team heroSection fix

--- src/components/Team/Hero.tsx
+++ src/components/Team/Hero.tsx
@@ -73,7 +73,7 @@ class Hero extends React.Component {
           minHeight: '300px'
         }}
       >
-        <Row type="flex" className="hero" align="bottom">
+        <Row type="flex" className="hero">
           <Col xs={24} sm={24} md={14} lg={12} xl={12} style={{ zIndex: '1' }}>
             <TextWrapper>
               <HeroText>The Team</HeroText>
@@ -97,7 +97,14 @@ class Hero extends React.Component {
               </HeroInfo>
             </TextWrapper>
           </Col>
-          <Col xs={0} sm={0} md={10} lg={12} xl={12}>
+          <Col
+            xs={0}
+            sm={0}
+            md={10}
+            lg={12}
+            xl={12}
+            style={{ display: 'grid', alignItems: 'end' }}
+          >
             <HeroArt>
               <img alt="team" src={header} />
             </HeroArt>
